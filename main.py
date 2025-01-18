a3fcb7d65484a537f0ececb3609d1850c6bc0649
Taleb88 <tdamaree18@gmail.com>
2025-01-18 11:24:04 -0500

    complete revision in progress

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import pandas as pd
+import time
 
 movies_df = pd.read_csv('movies.csv')
 ratings_df = pd.read_csv('ratings.csv')
 
-#print(ratings_df.head())
-#print(movies_df.head())
+movies_list = list(movies_df.columns)
+index = 0
+
+print('\nindex and columns in movies_df:')
+for cols in movies_list:
+    print(index, cols)
+    index += 1
+
+ratings_list = list(ratings_df.columns)
+index = 0
+
+print('\nindex and columns in ratings_df:')
+for cols in ratings_list:
+    print(index, cols)
+    index += 1
+
+start_time = time.time()
+print('\nread movies.csv and ratings.csv -', time.time() - start_time)
 
 # merge both movies_df and ratings_df based on movie_id column
 movies_merge_df = pd.merge(ratings_df, movies_df, on='movieId')
 
-print(movies_merge_df)
+print(f'\n{movies_merge_df}')
+
+print('\nratings_df and movies_df merge -', time.time() - start_time, '\n')
+
