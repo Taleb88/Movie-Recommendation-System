ecc1f20107e7b7a9fd59bbb7888b1f8eec6470ff
Taleb88 <tdamaree18@gmail.com>
2025-01-19 17:47:13 -0500

    update - enhancing user input feature by enabling the option to seek title(s) recommendation based on genres and/or ratings - continued progress

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ print(f'\nnum_of_ratings_per_movie:\n{num_of_ratings_per_movie_df.head(50)}')
 
 print('\nquanity of ratings per movie -', time.time() - start_time, '\n')
 
-# charts developed
+'''# charts developed
 sns.set_style('white')
 plt.figure(figsize=(8, 2))
 num_of_ratings_per_movie_df['rating'].hist(bins=70)
@@ -70,7 +70,7 @@ sns.jointplot(x=num_of_ratings_per_movie_df['rating'],
               height=15)
 plt.show()
 
-print('charts developed -', time.time() - start_time, '\n')
+print('charts developed -', time.time() - start_time, '\n')'''
 
 '''ratings_pivot_table = pd.pivot_table(movies_and_ratings_merged_df,
                                      values='rating',
@@ -81,26 +81,58 @@ print(ratings_pivot_table)
 
 print('ratings pivot table developed -', time.time() - start_time, '\n')'''
 
-movie_ratings_values = list(movies_and_ratings_merged_df['rating'].values)
+def filtering(df):
+    return df[(df['title'] == 'Three Colors: Red (Trois couleurs: Rouge) (1994)')]
+
+x = filtering(movies_and_ratings_merged_df)
+
+x.to_csv('test.csv')
+
+'''
 movie_genres_values = list(movies_and_ratings_merged_df['genres'].values)
+movie_ratings_values = list(movies_and_ratings_merged_df['rating'].values)
 
 while True:
     try: 
-        movie_genres_answer = input('please put in a name of a genre:\n')        
-        movie_ratings_answer = input('please put in a name of a rating:\n')
+        movie_genres_answer = input('\nplease put in a name of a genre:\n')        
+        movie_ratings_answer = input('\nplease put in a rating:\n')
         
         if movie_genres_answer == 'exit' or movie_ratings_answer == -1:
             break
 
-        if (movie_genres_answer in movie_genres_values) or (movie_ratings_answer in movie_ratings_values):
-            print(f'{movies_and_ratings_merged_df[(movies_and_ratings_merged_df['genres']==movie_genres_answer) 
-                                                  | (movies_and_ratings_merged_df['rating']==movie_ratings_answer)]}')
+        if (movie_genres_answer in movie_genres_values) and (movie_ratings_answer in movie_ratings_values):
+            print(f'{movies_and_ratings_merged_df[(movies_and_ratings_merged_df['genres']==movie_genres_answer) & \
+                                                  (movies_and_ratings_merged_df['rating']==movie_ratings_answer)]}')
         elif movie_genres_answer.isnumeric():
             print('please enter a string value only')
         elif not movie_ratings_answer.isnumeric():
             print('please enter a numerical value only')
-        elif (movie_ratings_answer not in movie_ratings_values) or (movie_genres_answer not in movie_genres_values):
-            print('title does not exist')
+        elif (movie_ratings_answer not in movie_ratings_values) and (movie_genres_answer not in movie_genres_values):
+            print('no result')
+    except ValueError:
+        print('please enter a valid title')
+        continue
+
+print('user input feature developed -', time.time() - start_time, '\n')'''
+
+movie_genres_values = list(movies_and_ratings_merged_df['genres'].values)
+movie_ratings_values = list(movies_and_ratings_merged_df['rating'].values)
+
+while True:
+    try: 
+        movie_genres_answer = input('\nplease put in a name of a genre:\n')        
+        movie_ratings_answer = float(input('\nplease put in a rating:\n'))
+        
+        if movie_genres_answer == 'exit' or movie_ratings_answer == -1:
+            break
+
+        if (movie_genres_answer in movie_genres_values) and (movie_ratings_answer in movie_ratings_values):
+            print(f'{movies_and_ratings_merged_df[(movies_and_ratings_merged_df['genres']==movie_genres_answer) & \
+                                                  (movies_and_ratings_merged_df['rating']==movie_ratings_answer)]}')
+        elif movie_genres_answer.isnumeric():
+            print('please enter a string value only')
+        elif (movie_ratings_answer not in movie_ratings_values) and (movie_genres_answer not in movie_genres_values):
+            print('no result')
     except ValueError:
         print('please enter a valid title')
         continue
